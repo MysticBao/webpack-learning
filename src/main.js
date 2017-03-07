afa9efa1fb09556107222845e2e4c712ea64bc4a
Mystic Weng <710120644@qq.com>
2017-03-07 23:03:48 +0800

    webpack init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// import notification from './Notification'
+
+// console.log(notification)
+// notification.announce('alert message')
+// notification.log('test')
+
+//require('./main.css')
+
+class Form {
+    constructor() {
+        // alert('Babel')
+        let numbers = [2,3,4].map(number => number * 2)
+        console.log(numbers)
+    }    
+}
+
+new Form()
